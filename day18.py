8a8ed33124d4ed784ac87174d497ecb76f9f0ef6
Ske <voltasalt@gmail.com>
2016-12-18 08:19:26 +0100

    Day 18 - Python

---
+++ day18.py
@@ -0,0 +1,31 @@
+import cProfile
+
+
+def next_row(previous_row):
+    last = ["."] + previous_row + ["."]
+    return ["^" if last[i] != last[i + 2] else "." for i in range(len(previous_row))]
+
+
+def get_rows(row):
+    yield row
+
+    while True:
+        row = next_row(row)
+        yield row
+
+
+def safe_count(starting, amount):
+    row_gen = get_rows(list(starting))
+
+    count = 0
+    for row in (next(row_gen) for _ in range(amount)):
+        count += row.count(".")
+
+    return count
+
+
+print("What is the initial row of traps?")
+starting = input(">")
+
+print(" - There are {} safe tiles in the first 40 rows -".format(safe_count(starting, 40)))
+print(" - There are {} safe tiles in the first 400000 rows -".format(safe_count(starting, 400000)))
